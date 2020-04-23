0401ebc04e9282840b332e705c1e6307d96cccfa
SwargamBhavyasri <63230302+SwargamBhavyasri@users.noreply.github.com>
2020-04-23 15:03:47 +0530

    Add files via upload

--- Case6/Operator_OLoad.cpp
+++ Case6/Operator_OLoad.cpp
@@ -1,9 +1,10 @@
 /*filename             : Operator_Oload.cpp
 Author                 : Swargam Bhavyasri
-Requirements           :  #include<iostream>,#include<string.h>
+Requirements           :  #include<iostream>,#include<string.h>,#include<stdlib.h>
 */  
 #include<iostream>
 #include<string.h>
+#include<stdlib.h>
 using namespace std;
 //class declaration
 class bank
@@ -17,42 +18,54 @@ public:
     bank()   
     {
         cout<<"Default Constructor :"<<endl;
+        
     }
-    //parameterised constructor
-    bank(int *i_no,char *cname)  
+    //parameterised constructor                                                          
+     bank(int *i_no,char *cname)  
     {
         cout<<"Parameterized Constructor is Invoked"<<endl;
-        iAc_No=i_no;
+         iAc_No=i_no;
         strcpy(cName,cname);
+    }
+    //copy constructor
+     bank(const bank& ob)
+    {
+    	iAc_No=new int;
+        strcpy(cName,ob.cName);
+        //copying variables
+        *iAc_No=*ob.iAc_No;
     }
     /*function name : Acc_Details
      parameters     : *ino,cname[]
      */
  void Acc_Details(int *ino,char cname[])
-    {
+    {   
         iAc_No=ino;
         strcpy(cName,cname);
     }
-
     void display()
     {
         cout<<"ID Number :"<<*iAc_No<<endl<<"Name:"<<cName<<endl;
         cout<<endl;
     }
-
-    void operator ++()  
+     //Assignment Operator Overloading
+    void operator =(const bank& k)
     {
-        ++(*iAc_No);
+        strcpy(cName,k.cName);
+        *iAc_No=*k.iAc_No;
     }
+    // destructor
     ~bank()
     {
-        cout<<"Destructor is Invoked"<<endl;
+        cout<<"Deallocating Memory"<<endl;
+       
     }
+   
 };
 int main(int argc,char* argv[])
 {
 	// condition for the arguments to display -h file
-    if(argc>=2) //condition for the arguments to display -h file
+    if(argc>=2) 
    {
    	if(strcmp(argv[1],"-h"))
    	        
@@ -64,29 +77,32 @@ int main(int argc,char* argv[])
     else
     {
     	//object creation for class 
-        bank b1; 
+        bank b; 
 		int iNum;
         char cNme[20];
-        cout<<"Enter Acc No of the A/C Holder:";
+         cout<<"Enter Acc No of the A/C Holder:";
         cin>>iNum;
         cout<<"Enter Name of the A/c Holder:";
-        getchar();
-        cin.get(cNme,100,'\n');
-        cout<<endl<<"Using Parameterized Constructor :"<<endl;
-        bank b2(&iNum,cNme);    
-        b2.display();
-        cout<<"After Operator Overloading"<<endl;
-        ++b2;     
-        cout<<"Using Member Function"<<endl;
-        b1.Acc_Details(&iNum,cNme);    
+        cin>>cNme;
+        b.Acc_Details(&iNum,cNme);
+        b.display();
+        //Parameterised Constructor
+         cout<<endl<<"Using Parameterized Constructor :"<<endl;
+         bank b1(&iNum,cNme);    
         b1.display();
-        //shallow copy 
-        bank b3=b1;
-        b1.display();
-		b3.display(); 
+        //copy Constructor
+        bank b2=b;
+        cout<<"After Using Copy Constructor"<<endl;
+         b2.display();
+        cout<<"Assignment Operator is Invoked"<<endl;
+         b2=b1;
+         b2.display();
+         b2=b;
+         b2.display();
         }
     return 0;
 }
+
 
  
 
