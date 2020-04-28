fa6b2f7bf862ce0006d63e69b531ecd32152b60a
SwargamBhavyasri <63230302+SwargamBhavyasri@users.noreply.github.com>
2020-04-28 18:42:34 +0530

    Add files via upload

--- Case6/Operator_OLoad.cpp
+++ Case6/Operator_OLoad.cpp
@@ -10,7 +10,7 @@ using namespace std;
 class bank
 {
 private:
-	//declaration of variables in private 
+//declaration of variables in private 
     int *iAc_No;               
     char cName[20]; 
 public:
@@ -18,47 +18,54 @@ public:
     bank()   
     {
         cout<<"Default Constructor :"<<endl;
-        
+        iAc_No=new int;
+        *iAc_No=0;
+        *cName='\0';
     }
-    //parameterised constructor                                                          
-     bank(int *i_no,char *cname)  
-    {
+     
+      //parameterised constructor                                                          
+	   bank(int i_no,char cname[20])  
+       {
         cout<<"Parameterized Constructor is Invoked"<<endl;
-         iAc_No=i_no;
+        iAc_No=new int;
+        *iAc_No=i_no;
         strcpy(cName,cname);
     }
     //copy constructor
      bank(const bank& ob)
     {
-    	iAc_No=new int;
+        iAc_No=new int;
         strcpy(cName,ob.cName);
         //copying variables
         *iAc_No=*ob.iAc_No;
     }
-    /*function name : Acc_Details
+     /*function name : Acc_Details
      parameters     : *ino,cname[]
      */
- void Acc_Details(int *ino,char cname[])
+    void Acc_Details(int ino,char cname[])
     {   
-        iAc_No=ino;
+       iAc_No=new int;
+        *this->iAc_No=ino;
         strcpy(cName,cname);
     }
     void display()
     {
-        cout<<"ID Number :"<<*iAc_No<<endl<<"Name:"<<cName<<endl;
+        cout<<"ID Number:"<<*iAc_No<<endl<<"Name:"<<cName<<endl;
         cout<<endl;
     }
      //Assignment Operator Overloading
-    void operator =(const bank& k)
+    void operator =(const bank& t)
     {
-        strcpy(cName,k.cName);
-        *iAc_No=*k.iAc_No;
+        delete iAc_No;
+        iAc_No=new int;
+        strcpy(cName,t.cName);
+        *iAc_No=*t.iAc_No;
     }
     // destructor
     ~bank()
     {
         cout<<"Deallocating Memory"<<endl;
-       
+        
     }
    
 };
@@ -77,32 +84,34 @@ int main(int argc,char* argv[])
     else
     {
     	//object creation for class 
-        bank b; 
-		int iNum;
+	int iNum;
         char cNme[20];
-         cout<<"Enter Acc No of the A/C Holder:";
+        //default constructor
+        bank *b=new bank();
+        b->display();
+        //parameterised constructor
+        bank *b1=new bank(19,"Bhavyasri");
+        b1->display();
+         //copy constructor invoked
+        bank *b2= new  bank(*b1);
+        cout<<"After using Copy Construcotr"<<endl;
+        b2->display();
+	cout<<"Enter Acc No of the A/C Holder:";
         cin>>iNum;
         cout<<"Enter Name of the A/c Holder:";
         cin>>cNme;
-        b.Acc_Details(&iNum,cNme);
-        b.display();
-        //Parameterised Constructor
-         cout<<endl<<"Using Parameterized Constructor :"<<endl;
-         bank b1(&iNum,cNme);    
-        b1.display();
-        //copy Constructor
-        bank b2=b;
-        cout<<"After Using Copy Constructor"<<endl;
-         b2.display();
-        cout<<"Assignment Operator is Invoked"<<endl;
-         b2=b1;
-         b2.display();
-         b2=b;
-         b2.display();
+        b1->Acc_Details(iNum,cNme);
+        cout<<"After Assigning the values ::"<<endl;
+        b2->display();
+        b1->display();
+        cout<<"Assignment Operator Invoked ::"<<endl;
+         b1=b2;
+        b2->~bank();
+        b1->display();
+        b2=b;
+        cout<<"After Assignment :: "<<endl;
+        b2->display();
+        b1->display(); 
         }
     return 0;
 }
-
-
- 
-
