ead78c8d2589de437e1b653fa3626d64754bde6f
SwargamBhavyasri <63230302+SwargamBhavyasri@users.noreply.github.com>
2020-05-19 14:10:16 +0530

    Add files via upload

--- Case4/Mobile_keypad.cpp
+++ Case4/Mobile_keypad.cpp
@@ -1,85 +1,76 @@
-/* Filename         :    Mobile_keypad.cpp
+/*/* Filename         :    Mobile_keypad.cpp
    Description      :   Program to print all possible combinations of words for digits in a phone keypad
    Creation Author  :  Swargam Bhavyasri
    */
-#include <iostream>
-#include <map>
-#include <string.h>
+#include<iostream>
+#include<vector>
+#include<string.h>
 using namespace std;
-string dial(int key)
-{ 
-    //creates a object for map is m
-    map<int,string>m;
-    m[0]=" ";
-    m[1]=" ";
-    m[2]="abc";
-    m[3]="def";
-    m[4]="ghi";
-    m[5]="jkl";
-    m[6]="mno";
-    m[7]="pqrs";
-    m[8]="tuv";
-    m[9]="wxyz";
-   return m[key];
-}
 //class declaration
 class Mobile
 {
-	public:
-		/* function name   : phoneKey
-		   Return Type     : void*/
-    	void phoneKey(int iNum,string output)
-       {
-    	//condition to check if number is less than 0
-        if(iNum<0)
-        {
-		  cout<<"Enter only Positive Numbers ::"<<endl;
-       	}
-       	//condition to check if number is 0 or 1
-        else if(iNum==0||iNum==1)
-        {
-           	 cout<<output<<"  ";
-              return;
-         }
-         else
-         {
-            string keypad=dial(iNum%10);
-            for(int iIndex=0;iIndex<keypad.size();iIndex++)
-             {
-                phoneKey(iNum/10,keypad[iIndex]+output);
-                  }
-           }
-}
-/* function name   :  printKeypad
-   Return type     : void */
-public :void printKeypad(int iNum)
-{
+private:
+	//creates a object for vector 
+    vector<string> output;
+    /*function name     :   generateHelper
+      Return type       :   void*/
+    void generateHelper(string letter_combinations[],string& sCurrent,int iIndex,string& sDigits,vector<string> &output)
+    {
+    	
+            if (iIndex == sDigits.size())
+            {
+            	//if equal push the answer to current digit
+                output.push_back(sCurrent);
+            }
+            else
+            {
+			
+            //storing the digit in int form
+            int digit = sDigits[iIndex] - '0';
+                //for looop runs until the value less than the digit size
+                for (int i = 0; i < letter_combinations[digit].size(); i++)
+                {
+                    	
+                  sCurrent.push_back(letter_combinations[digit][i]);
+                  //generate the combinations of digits
+                 generateHelper(letter_combinations,sCurrent,iIndex + 1,sDigits,output);
+                 sCurrent.pop_back();
+                }   
+            }
+        }
     
-    //  Given an integer number print all the possible combinations of the keypad
-     phoneKey(iNum," ");
-    
-}
+    public:
+        vector<string> letterCombinations(string digits)
+        {
+            string sCurrent="";
+            //letter combinations
+            string digits_letter_combination_map [10] = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+            generateHelper(digits_letter_combination_map,sCurrent,0,digits,output);
+            return output;
+        }
 };
 int main(int argc,char *argv[])
-{
-	//create a object for class
-	Mobile m;
-	//used for help command 
-	if((argc==2)&&strcmp(argv[1],"-h")==0)
+{  
+    //help command 
+ 	if((argc==2)&&strcmp(argv[1],"-h")==0)
     {
         cout<<"Usage  ::"<<endl;
         cout<<"Input :: Positive Integers from 2 to 9"<<endl;
      }
     else
     { 
-	 //declaration of variable   
-	 int iValue;
-	 //Read the value
-	 cin>>iValue;
-	 //function call
-	 m.printKeypad(iValue);
-      return 0;
-
+    
+    //object creation for class
+   	Mobile m;
+   	//variable declaration
+   	string digits;
+	//Reading the digits
+	cin>>digits;
+	vector<string> final_result=m.letterCombinations(digits);
+	//for loop used for final result
+	for (int i = 0; i < final_result.size(); i++)
+                cout << final_result[i] << " ";
+    cout<<endl;
+	return 0;
 }
 }
-
